568a95faf4c3eb55f7c0ea1437d91fba944b1ab3
hill1942 <ykdacd123@gmail.com>
2014-09-01 12:52:53 +0800

    wrap by AMD

--- Utils/misc.js
+++ Utils/misc.js
@@ -1,22 +1,27 @@
 /**
  * @fileOverview
  * @author kaidi.ykd
- * @date 2014/8/31
  */
 
-
-var remove_duplication = function(array) {
-    var diffs = [];
-    diffs.push(array[0]);
-    for (var i = 1; i < array.length; i++) {
-        for (var j = 0; j < diffs.length; j++) {
-            if (array[i] == diffs[j]) {
-                break;
-            }
-            if (j == diffs.length - 1) {
-                diffs.push(array[i]);
+define(function() {
+    var remove_duplication = function(array) {
+        var diffs = [];
+        diffs.push(array[0]);
+        for (var i = 1; i < array.length; i++) {
+            for (var j = 0; j < diffs.length; j++) {
+                if (array[i] == diffs[j]) {
+                    break;
+                }
+                if (j == diffs.length - 1) {
+                    diffs.push(array[i]);
+                }
             }
         }
+        return diffs;
+    };
+
+    return {
+        rmDuplicate: remove_duplication
     }
-    return diffs;
-};
+});
+
